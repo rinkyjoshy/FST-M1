d98b3b0323918911ffc66e54f0834a4c7fb5d84c
rinkyjoshy <88023635+rinkyjoshy@users.noreply.github.com>
2021-08-11 21:17:46 +0530

    Add files via upload

---
+++ Python/Activities/Activity2.py
@@ -0,0 +1,8 @@
+number =  int(input("Please provide a number"))
+print(number)
+if (number % 2) == 0 :
+    print("Number is even")
+else:
+    print("Number is odd")
+
+
---
+++ Python/Activities/helloworld.py
@@ -0,0 +1,5 @@
+a = 5
+print(a)
+b = 7
+print(b)
+print(a+b)
