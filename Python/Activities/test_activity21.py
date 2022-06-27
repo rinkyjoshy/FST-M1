974836d152c5beada2fc4e6b999286c55d45b137
rinkyjoshy <88023635+rinkyjoshy@users.noreply.github.com>
2022-06-27 18:31:06 +0530

    Add files via upload

---
+++ Python/Activities/test_activity21.py
@@ -0,0 +1,28 @@
+
+
+def test_sum():
+    num1 = 10
+    num2 = 15
+
+    assert num1 + num2 == 25
+
+def test_diff():
+    num3 = 20
+    num4 = 10
+
+    assert num3-num4 == 10
+
+def test_product():
+    num5 = 5
+    num6 = 4
+
+    assert num5 * num6 == 20
+
+def test_quotient():
+    num7 = 10
+    num8 = 5
+
+    assert num7 /num8 == 2
+
+
+
